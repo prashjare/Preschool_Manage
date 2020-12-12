7ca7f20cb274b52c76de3e20a5d0b217bf24f890
Prashant <prashantjare@gmail.com>
2020-12-12 14:00:38 +0100

    Initial commit

---
+++ src/main/java/com/cloudtech/Preschool_Manage/PreschoolManageApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtech.Preschool_Manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreschoolManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreschoolManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudtech/Preschool_Manage/PreschoolManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtech.Preschool_Manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreschoolManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
